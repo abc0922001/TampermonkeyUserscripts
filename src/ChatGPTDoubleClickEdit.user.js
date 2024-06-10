24a550daf5b1bc4d0537c546b1031f1c3d5672c7
abc0922001 <abc0922001@hotmail.com>
2024-06-10 15:07:59 +0800

    ChatGPTDoubleClickEdit fix not work

--- src/ChatGPTDoubleClickEdit.user.js
+++ src/ChatGPTDoubleClickEdit.user.js
@@ -1,41 +1,59 @@
 // ==UserScript==
 // @name         ChatGPT: 滑鼠雙擊編輯提示文字
-// @version      1.0.2
+// @version      1.0.3
 // @description  滑鼠雙擊先前已經輸入的提示就可直接編輯
 // @license      MIT
 // @homepage     https://blog.miniasp.com/
 // @homepageURL  https://blog.miniasp.com/
 // @website      https://www.facebook.com/will.fans
-// @source       https://github.com/doggy8088/TampermonkeyUserscripts/raw/main/src/ChatGPTDoubleClickEdit.user.js
-// @namespace    https://github.com/doggy8088/TampermonkeyUserscripts/raw/main/src/ChatGPTDoubleClickEdit.user.js
-// @match        *://chat.openai.com/*
-// @author       Will Huang
+// @source       https://github.com/abc0922001/TampermonkeyUserscripts/raw/main/src/ChatGPTDoubleClickEdit.user.js
+// @namespace    https://github.com/abc0922001/TampermonkeyUserscripts/raw/main/src/ChatGPTDoubleClickEdit.user.js
+// @match        *://chatgpt.com/*
 // @run-at       document-idle
+// @author       Will Huang,abc0922001
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=openai.com
 // ==/UserScript==
 
-
 (async function () {
-    'use strict';
+    "use strict";
 
-    const checkForMainElement = setInterval(() => {
-        if (document.getElementsByTagName('main').length > 0) {
-            // 由於在切換歷史紀錄時會重建 main 元素，所以要監聽 document.body 的事件
-            document.body.addEventListener('dblclick', (event) => {
-                // 提示的 DOM 都套用 empty:hidden 這個類別
-                if (event.target.className == 'empty:hidden') {
-                    // 由於 ChatGPT 網站上的 DOM 都沒有定位點，所以只能靠 SVG 的線條來決定是哪一個按鈕
-                    // 底下這個線條是編輯按鈕的「鉛筆」圖示
-                    let svg = event.target.parentElement.parentElement.parentElement.querySelector('path[d*=\'M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z\']');
+    function initListener() {
+        document.body.addEventListener("dblclick", (event) => {
+            let target = event.target;
+
+            // 確保 target 是一個元素節點
+            while (target && target.nodeType !== Node.ELEMENT_NODE) {
+                target = target.parentElement;
+            }
+
+            if (target) {
+                // 尋找最近包含 svg 鉛筆圖示的按鈕
+                const container = target.closest(".relative");
+                if (container) {
+                    const svgPath =
+                        "path[fill-rule='evenodd'][d='M13.293 4.293a4.536 4.536 0 1 1 6.414 6.414l-1 1-7.094 7.094A5 5 0 0 1 8.9 20.197l-4.736.79a1 1 0 0 1-1.15-1.151l.789-4.736a5 5 0 0 1 1.396-2.713zM13 7.414l-6.386 6.387a3 3 0 0 0-.838 1.628l-.56 3.355 3.355-.56a3 3 0 0 0 1.628-.837L16.586 11zm5 2.172L14.414 6l.293-.293a2.536 2.536 0 0 1 3.586 3.586z'][clip-rule='evenodd']";
+                    const svg = container.querySelector(svgPath);
                     if (svg) {
-                        let btn = svg.parentElement.parentElement;
-                        btn.click();
+                        const btn = svg.closest("button");
+                        if (btn) {
+                            btn.click();
+                        }
                     }
                 }
-            });
-            console.log('ChatGPT: 滑鼠雙擊編輯提示文字 Initialized');
-            clearInterval(checkForMainElement);
-        }
-    }, 500);
+            }
+        });
+        console.log("ChatGPT: 滑鼠雙擊編輯提示文字 Initialized");
+    }
 
+    const observer = new MutationObserver((mutations, obs) => {
+        if (document.getElementsByTagName("main").length > 0) {
+            initListener();
+            obs.disconnect();
+        }
+    });
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true,
+    });
 })();
